316a1c70104556e37c5fd89a9aae57e3537bfb31
ATOMLubover <13809006129@163.com>
2026-05-29 21:57:44 +0800

    feat: accept trait

--- src/api/http/handler/authorization.rs
+++ src/api/http/handler/authorization.rs
@@ -2,15 +2,14 @@ use axum::Json;
 use axum::extract::State;
 
 use crate::api::harness::Harness;
-use crate::api::http::handler::HttpResl;
-use crate::api::http::handler::result::accept;
+use crate::api::http::handler::HttpResult;
+use crate::api::http::handler::result::Accept as _;
 use crate::usecase;
 use crate::usecase::value_object::user::{SignUpUserParams, SignUpUserReply};
 
 pub async fn sign_up_user(
     State(harn): State<Harness>,
     Json(params): Json<SignUpUserParams>,
-) -> HttpResl<SignUpUserReply> {
-    let reply = usecase::user::sign_up_user(&harn, params).await?;
-    accept(reply)
+) -> HttpResult<SignUpUserReply> {
+    usecase::user::sign_up_user(&harn, params).await?.accept()
 }
