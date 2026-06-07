2b37a6cac03d499a1c74e2e13387d4c7510dca01
ATOMLubover <13809006129@163.com>
2026-06-07 14:55:23 +0800

    chores: log init

--- src/main.rs
+++ src/main.rs
@@ -6,17 +6,44 @@ use poprako_r::harness::Harness;
 use poprako_r::infra::external::image_pool::OssImagePool;
 use poprako_r::infra::external::token::JwtIssuer;
 use poprako_r::infra::query::RdbQuery;
-use tracing_subscriber::layer::SubscriberExt as _;
-use tracing_subscriber::util::SubscriberInitExt as _;
 
 #[tokio::main]
 async fn main() {
     dotenvy::dotenv().ok();
 
-    tracing_subscriber::registry()
-        .with(tracing_subscriber::fmt::layer())
-        .with(tracing_subscriber::EnvFilter::from_default_env())
-        .init();
+    use tracing_subscriber::layer::SubscriberExt as _;
+    use tracing_subscriber::util::SubscriberInitExt as _;
+
+    #[cfg(debug_assertions)]
+    {
+        tracing_subscriber::registry()
+            .with(tracing_subscriber::fmt::layer())
+            .with(tracing_subscriber::EnvFilter::from_default_env())
+            .init();
+    }
+    #[cfg(not(debug_assertions))]
+    {
+        use tracing_subscriber::filter::LevelFilter;
+
+        let log_folder = std::path::Path::new("logs");
+        std::fs::create_dir_all(log_folder).expect("Failed to create log folder");
+
+        let file_appender = tracing_appender::rolling::daily(log_folder, "poprako_r.log");
+        let (non_blocking, _guard) = tracing_appender::non_blocking(file_appender);
+
+        tracing_subscriber::registry()
+            .with(
+                tracing_subscriber::fmt::layer()
+                    .json()
+                    .with_writer(non_blocking)
+                    .with_ansi(false),
+            )
+            .with(tracing_subscriber::EnvFilter::new(LevelFilter::INFO.to_string()))
+            .init();
+
+        // Keep the guard alive for the entire program lifetime.
+        std::mem::forget(_guard);
+    }
 
     let config = AppConfig::from_default_file()
         .await
